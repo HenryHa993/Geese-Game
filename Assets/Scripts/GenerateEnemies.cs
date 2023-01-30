d02f7f50c9520a1316b000c0abc8c8178db6a1c7
EvenJoels <Joelevans1305@gmail.com>
2023-01-30 18:52:22 +0000

    added enemy delay

--- Assets/Scripts/GenerateEnemies.cs
+++ Assets/Scripts/GenerateEnemies.cs
@@ -8,7 +8,8 @@ public class GenerateEnemies : MonoBehaviour
     public GameObject Enemy, outerLayerNode, player, generateBoard;
     public GameObject[] enemyArray;
     public bool isTurn, movesMade;
-    public int numEnemies = 5;
+    public int numEnemies = 5, moveDelayInFrames = 100;
+    private int frameCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +31,7 @@ public class GenerateEnemies : MonoBehaviour
                 currentNode = currentNode.GetComponent<Point>().sibling.GetComponent<Point>().sibling;
             }
         }
-        
+        frameCounter = 0;
     }
 
     // Update is called once per frame
@@ -38,24 +39,35 @@ public class GenerateEnemies : MonoBehaviour
     {
         if (isTurn)
         {
-            //move enemies
-            System.Random rand = new System.Random();
-            int numMoved = rand.Next(numEnemies);
-
-            for (int i = 0; i < numMoved; i++)
+            if (frameCounter >= moveDelayInFrames)
             {
-                Enemy enemy = enemyArray[i].GetComponent<Enemy>();
-                enemy.updateMoveScores();
-                int index =  System.Array.IndexOf(enemy.moveScores, enemy.moveScores.Min());
-                
+                frameCounter = 0;
+
+                //find minScores for all valid moves
+                    //  Have BestMove class that considers minScore, corresponding move, and enemy
+                //select random number of enemies to move
+                //move enemies
+                System.Random rand = new System.Random();
+                int numMoved = rand.Next(numEnemies);
+
+                for (int i = 0; i < numMoved; i++)
+                {
+                    Enemy enemy = enemyArray[i].GetComponent<Enemy>();
+                    enemy.updateMoveScores();
+                    int index =  System.Array.IndexOf(enemy.moveScores, enemy.moveScores.Min());
+                    
 
 
 
-                GameObject newNode = enemy.possibleMoves[index];
-                enemy.transform.position = newNode.transform.position;
-                enemy.entityNode = newNode;
+                    GameObject newNode = enemy.possibleMoves[index];
+                    enemy.transform.position = newNode.transform.position;
+                    enemy.entityNode = newNode;
+                }
+                movesMade = true;
+            }else
+            {
+                frameCounter++;
             }
-            movesMade = true;
         }
             
     }
