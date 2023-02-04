81a44a9e62ad66a2b9d0ec2e4df696e2e5ddb0a8
EvenJoels <Joelevans1305@gmail.com>
2023-02-04 12:07:32 +0000

    Added lowering of points

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Point : MonoBehaviour
 {
+    public float yUpValue = 0f, yDownValue = -0.2f;
     public int layer;
     public int sidePosition;
+    public bool isVisible;
     public GameObject child; // higher level
     public GameObject parent; // lower level
     public GameObject sibling; // L if layer 0dd, R is layer even
     public GameObject badSibling;
     public GameObject isOccupiedBy;
+
+    private void Update() {
+        transform.position = new Vector3(transform.position.x, (isVisible)?yUpValue:yDownValue,transform.position.z);
+    }
 }
